07e29bf8b659a37f57b7fa9f9e2e15b5290819cd
Karina <demi4815@gmail.com>
2020-10-25 18:04:53 +0300

    Промежуточный результат (2)

--- com/company/Expression.java
+++ com/company/Expression.java
@@ -17,14 +17,64 @@ public class Expression
         Str = null;
     }
 
-    Stack<Double> Operands = new Stack<>();
-    Stack<Character> Functions = new Stack<>();
-    int pos = 0;
-    Object Token = new Object();
-    Object PrevToken = new Object();
 
 
-    void PopFunction(Stack<Double> Operands, Stack<Character> Functions)
+    static double Calculate(String expr) throws Exception
+    {
+        Stack<Double> Operands = new Stack<>();
+        Stack<Character> Functions = new Stack<>();
+        int pos = 0;
+        char Token = ' ';
+        char PrevToken = ' ';
+
+        char[] expression =  expr.toCharArray(); //ПОСТАВИТЬ СКОБКИ В НАЧАЛЕ И КОНЦЕ
+        //ArrayList<Character> expression = expr.toCharArray;
+
+
+        do
+        {
+            Token = expression[pos];
+
+            if (PrevToken =='(' && Token == '-') {
+                Operands.push(0.0);
+            }
+
+
+            if (Character.isDigit(Token))
+            {
+                Operands.push((double)Token);
+            }
+
+            else if (Character.isSpaceChar(Token))
+            {
+                if ((char)Token == ')')
+                {
+                    while (Functions.size() > 0 && Functions.peek() != '(')
+                        PopFunction(Operands, Functions);
+                    Functions.pop(); // Удаляем саму скобку "("
+                }
+                else
+                {
+                    while (CanPop((char)Token, Functions)) // Если можно вытолкнуть
+                        PopFunction(Operands, Functions); // то выталкиваем
+                    Functions.push((char)Token); // Кидаем новую операцию в стек
+                }
+            }
+            PrevToken = Token;
+            pos++;
+
+        } while (pos + 1 <= expression.length);
+
+        if (Operands.size() > 1 || Functions.size() > 0)
+            throw new Exception("Ошибка в разборе выражения");
+
+        return Operands.pop();
+
+
+    }
+
+
+    static void PopFunction(Stack<Double> Operands, Stack<Character> Functions)
     {
         double B = Operands.pop();
         double A = Operands.pop();
@@ -36,7 +86,7 @@ public class Expression
         }
     }
 
-    int GetPriority(char op) throws Exception {
+    static int GetPriority(char op) throws Exception {
         switch (op)
         {
             case '(':
@@ -51,7 +101,7 @@ public class Expression
     }
 
 
-    boolean CanPop(char op1, Stack<Character> Functions) throws Exception {
+    static boolean CanPop(char op1, Stack<Character> Functions) throws Exception {
         if (Functions.empty())
             return false;
         int p1 = GetPriority(op1);
