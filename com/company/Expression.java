5b3164cfe5510417049de8dcbe4903c86da095db
Karina <demi4815@gmail.com>
2020-10-25 21:24:44 +0300

    Добавлены некоторые комментарии

--- com/company/Expression.java
+++ com/company/Expression.java
@@ -4,48 +4,50 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Stack;
 
+//Алгоритм основан на алгоритме Дейкстры для вычисления значения арифметического выражения (с помощью двух стеков)
+
 public class Expression
 {
 
-    static ArrayList<Character> simpleFunctions = new ArrayList<>(
+    static ArrayList<Character> simpleFunctions = new ArrayList<>( //Массив простых операций
             Arrays.asList( '/', '*', '+', '-')
     );
 
-    static double Calculate(String expr) throws Exception
+    static double Calculate(String expr) throws Exception //Основная функция для вычиления выражения
     {
-        Stack<Double> Operands = new Stack<>();
-        Stack<Character> Functions = new Stack<>();
+        Stack<Double> Operands = new Stack<>(); //Стек для операндов
+        Stack<Character> Functions = new Stack<>(); //Стек для операций
         int pos = 0;
         char Token = ' ';
-        char PrevToken = 'j';
+        char PrevToken = 'я'; //Символ, который точно не операнд и не функция
 
         ArrayList<Character> expression = new ArrayList<>();
 
-        for (int i = 0; i < expr.length(); i++) {
+        for (int i = 0; i < expr.length(); i++) { //Копируем в массив выражение посимвольно
             expression.add(expr.charAt(i));
         }
 
-        expression.add(0, '(');
-        expression.add(expression.size(), ')');
+        expression.add(0, '('); //Скобка в начале
+        expression.add(expression.size(), ')'); // И скобка в конце для правильного вычисления
 
         do
         {
             Token = expression.get(pos);
 
-            if (PrevToken =='(' && Token == '-') {
+            if (PrevToken =='(' && Token == '-') { //Если после скобки идет операнд со знаком минус
                 Operands.push(0.0);
             }
 
 
-            if (Character.isDigit(Token))
+            if (Character.isDigit(Token)) //Если Token число, то добавляем его в стек для операндов
             {
                 int t = Character.getNumericValue(Token);
                 Operands.push((double)t);
             }
 
-            if (simpleFunctions.contains(Token) || Token == '(' || Token == ')' )
+            if (simpleFunctions.contains(Token) || Token == '(' || Token == ')' ) //Если Token операция
             {
-                if ((char)Token == ')')
+                if ((char)Token == ')') // Выталкиваем все операции до первой открывающейся скобки
                 {
                     while (Functions.size() > 0 && Functions.peek() != '(')
                         PopFunction(Operands, Functions);
@@ -54,7 +56,7 @@ public class Expression
                 else
                 {
                     while (CanPop((char)Token, Functions)) // Если можно вытолкнуть
-                        PopFunction(Operands, Functions); // то выталкиваем
+                        PopFunction(Operands, Functions); // То выталкиваем
                     Functions.push((char)Token); // Кидаем новую операцию в стек
                 }
             }
@@ -70,7 +72,7 @@ public class Expression
     }
 
 
-    static void PopFunction(Stack<Double> Operands, Stack<Character> Functions)
+    static void PopFunction(Stack<Double> Operands, Stack<Character> Functions) //Операция над двумя последними операндами
     {
         double B = (double)Operands.pop();
         double A = (double)Operands.pop();
@@ -87,7 +89,7 @@ public class Expression
         }
     }
 
-    static int GetPriority(char op) throws Exception {
+    static int GetPriority(char op) throws Exception { //Приоритет операции
         switch (op)
         {
             case '(':
@@ -102,7 +104,7 @@ public class Expression
     }
 
 
-    static boolean CanPop(char op1, Stack<Character> Functions) throws Exception {
+    static boolean CanPop(char op1, Stack<Character> Functions) throws Exception { //Можно ли вытолкнуть операнд
         if (Functions.empty())
             return false;
         int p1 = GetPriority(op1);
